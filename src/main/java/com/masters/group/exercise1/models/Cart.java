6481eb47738c14fc117a847575024a2f9a3aef18
atdjrothers <atdjrothers@gmail.com>
2021-12-06 10:42:03 +0800

    cart object

--- src/main/java/com/masters/group/exercise1/models/Cart.java
+++ src/main/java/com/masters/group/exercise1/models/Cart.java
@@ -4,8 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    private static final List<Order> orders = new ArrayList<>();
+    private final List<Order> orders = new ArrayList<>();
 
     private double totalAmount;
 
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void addOrder(Order order){
+        orders.add(order);
+    }
 }
