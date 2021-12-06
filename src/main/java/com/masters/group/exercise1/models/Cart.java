699a46eca1454de0de03bd38d97df295647876e0
atdjrothers <atdjrothers@gmail.com>
2021-12-06 10:40:47 +0800

    display items

---
+++ src/main/java/com/masters/group/exercise1/models/Cart.java
@@ -0,0 +1,11 @@
+package com.masters.group.exercise1.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private static final List<Order> orders = new ArrayList<>();
+
+    private double totalAmount;
+
+}
